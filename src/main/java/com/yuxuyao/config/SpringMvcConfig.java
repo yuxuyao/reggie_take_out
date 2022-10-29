8035ced0c130120ccd9d2307a8f5985de51ffbb3
yxy <1634442473@qq.com>
2022-10-29 14:07:07 +0800

    加入redis，缓存验证码和菜品，并使用Spring

--- src/main/java/com/yuxuyao/config/SpringMvcConfig.java
+++ src/main/java/com/yuxuyao/config/SpringMvcConfig.java
@@ -2,11 +2,9 @@ package com.yuxuyao.config;
 
 import com.yuxuyao.utils.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
